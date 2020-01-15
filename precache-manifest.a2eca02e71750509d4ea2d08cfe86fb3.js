52da945e0f48a1c5b160c8e787f6e0a6d87a858f
AbdulrhmanJo <Aalhani19@gmail.com>
2020-01-15 17:16:12 +0300

    Updates

---
+++ precache-manifest.a2eca02e71750509d4ea2d08cfe86fb3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611d92b3736fb6fdb6cfc477c1e53c18",
+    "url": "/myreads/index.html"
+  },
+  {
+    "revision": "3e68965e3e6603edd85b",
+    "url": "/myreads/static/css/main.e5a1ea64.chunk.css"
+  },
+  {
+    "revision": "ae993247b73a0bf7a203",
+    "url": "/myreads/static/js/2.28436244.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/myreads/static/js/2.28436244.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e68965e3e6603edd85b",
+    "url": "/myreads/static/js/main.04513f8b.chunk.js"
+  },
+  {
+    "revision": "143dd165e83cb84c5936",
+    "url": "/myreads/static/js/runtime-main.ed292944.js"
+  },
+  {
+    "revision": "c25f4472d8f2bb0ad57eb251e13d9c54",
+    "url": "/myreads/static/media/abstract-searching.c25f4472.png"
+  },
+  {
+    "revision": "6705a44bdc5309d0e625eb7c8f97a492",
+    "url": "/myreads/static/media/book-of-black-cover-closed.6705a44b.svg"
+  },
+  {
+    "revision": "b731be8186da88ce639eefd237c4cad3",
+    "url": "/myreads/static/media/clip-bad-gateaway.b731be81.png"
+  },
+  {
+    "revision": "a9765fb4147f9eb4dfa10ac8cb8204d8",
+    "url": "/myreads/static/media/clip-education.a9765fb4.png"
+  },
+  {
+    "revision": "7217f10c2d6ea4280ef705201ac9c5a6",
+    "url": "/myreads/static/media/dashboard.7217f10c.svg"
+  },
+  {
+    "revision": "13a892e8f581b3b4e805414b2d0093d2",
+    "url": "/myreads/static/media/left-arrow.13a892e8.svg"
+  },
+  {
+    "revision": "28f7c2350ad7dbd214cb0d0203b5c7c2",
+    "url": "/myreads/static/media/right-arrow.28f7c235.svg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/myreads/static/media/search.2ea4c436.svg"
+  }
+]);
