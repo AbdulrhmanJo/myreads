687fdddc88ed72d3f27cf179be90da2530f31496
AbdulrhmanJo <Aalhani19@gmail.com>
2020-01-16 13:35:59 +0300

    add basename to solve githup pages problem

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/myreads">
         <App />
     </BrowserRouter>
     ,document.getElementById('root'));
